f462b999503b24d0043dc938d56f7ec4f995030c
Damien Pobel <dp@ez.no>
2013-06-29 12:25:02 +0200

    hanged: put the ResultView component into a separate file

---
+++ hello-ez/js/result-view.js
@@ -0,0 +1,11 @@
+YUI.add('result-view', function (Y) {
+    "use strict";
+
+    Y.ResultView = Y.Base.create('resultView', Y.TemplateView, [], {
+        events: {
+        }
+    }, {
+
+    });
+
+}, '0.0.1');
